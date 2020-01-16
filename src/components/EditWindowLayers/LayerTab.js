f3d6a2c975d4ef66ae6e3da7240f370f7a1069dc
Luke Peña <luketpena@gmail.com>
2020-01-16 12:40:45 -0600

    Drag layers mostly done.

--- src/components/EditWindowLayers/LayerTab.js
+++ src/components/EditWindowLayers/LayerTab.js
@@ -5,6 +5,7 @@ import { SvgIcon } from '@material-ui/core';
 import {useDispatch, useSelector} from 'react-redux';
 
 const Container = styled.div`
+position: relative;
   border-radius: 8px;
   padding: 8px;
   margin-bottom: 8px;
@@ -25,6 +26,33 @@ const Title = styled.p`
   line-height: 24px;
 `;
 
+const DeletePath = styled.path`
+  fill: var(--color-text-dark);
+  
+`;
+
+const DeleteArea = styled.div`
+  height: 100%;
+  top: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  padding-left: 12px;
+  position: absolute;
+  right: 4px;
+
+  &:hover path{
+    fill: var(--color-text-light);
+  }
+`;
+
+const SelectArea = styled.div`
+  width: 75%;
+  text-align: left;
+`;
+
+const DeleteButton = styled.button``;
+
 export default function Task(props) {
 
   const dispatch = useDispatch();
@@ -35,6 +63,16 @@ export default function Task(props) {
         <path className='layer-drag-path' d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
       </SvgIcon>
     );
+  }
+
+  function DeleteIcon(props) {
+    return (
+      <DeleteArea>
+        <SvgIcon {...props} viewBox='0 0 48 24'>
+          <DeletePath d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+        </SvgIcon>
+      </DeleteArea>
+    )
   }
 
   const select = useSelector(state=>state.edit.select);
@@ -60,7 +98,10 @@ export default function Task(props) {
           index={props.index}
         >
           <div {...provided.dragHandleProps}><DragIcon/></div>
-          <Title isDragging={snapshot.isDragging} >{props.layer.layer_name}</Title>
+          <SelectArea>
+            <Title isDragging={snapshot.isDragging} >{props.layer.layer_name}</Title>
+          </SelectArea>
+          <DeleteIcon />
         </Container>
       )}
     </Draggable>
